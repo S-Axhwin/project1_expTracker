ebc9f788b231367af1e9dd8a688d42ab7e781cb4
S-Axhwin <s.ashwin.sathiyamoorthy.d@gmail.com>
2024-01-12 15:57:07 +0530

    UI Completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-
+import Header from './components/Header'
+import Balance from './components/Balance';
+import History from './components/History'
+import AddTrans from './components/AddTrans';
+import './app.css'
 
 function App() {
   return (
     <div>
-      
+      <Header/>
+      <div className='contaienr'>
+        <Balance/>
+        <History/>
+        <AddTrans/>
+      </div>
     </div>
   );
 }
